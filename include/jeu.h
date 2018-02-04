ba0c96dcd35fe53a27dbfd82b10b3f433471f3f8
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-02-04 22:07:45 +0100

    Update jeu.h

--- include/jeu.h
+++ include/jeu.h
@@ -1,1 +1,17 @@
+#ifndef _JEU_H_
+#define _JEU_H_
+#include "commun.h"
 
+/* jeu de cartes */
+typedef struct jeu_s {
+	int nb;
+	carte_t * liste[N];
+} jeu_t;
+
+jeu_t * jeu_creer(void);
+void jeu_detruire(jeu_t ** jeu);
+void jeu_initialiser(jeu_t * jeu);
+void jeu_afficher(jeu_t * jeu);
+void jeu_melanger(jeu_t * jeu);
+
+#endif
