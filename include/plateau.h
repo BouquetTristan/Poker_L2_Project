ff23220d8ece05e6ecdcaff4349a2022ec81d2af
Tristan <tri.bouquet@laposte.net>
2018-02-05 10:47:11 +0100

    Add the board's file with structures of the board and players

---
+++ include/plateau.h
@@ -0,0 +1,17 @@
+#ifndef _PLATEAU_H_
+#define _PLATEAU_H_
+#include "carte.h"
+
+typedef struct player_s
+{
+	carte_t * cartePlayer[5];
+	int token;
+	char name[20];
+};
+
+typedef struct plateau_s
+{
+	player_t * player[4];
+	int pot;
+};
+
