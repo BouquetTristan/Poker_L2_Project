4e51c875522d43871c08cb1dffbb51687eea41fb
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-02-05 11:35:14 +0100

    Add files via upload

---
+++ joueur.c
@@ -0,0 +1,20 @@
+#include "joueur.h"
+
+player_t * joueur_creer(void) {
+	int i;
+	player_t * joueur = malloc(sizeof(player_t));
+	joueur->name = malloc(sizeof(char));
+	joueur->token = 0;
+	for(i = 0; i < NB_PLAYER; i++)
+	 joueur->cartePlayer[i] = carte_creer();
+}
+
+void joueur_detruire(player_t ** joueur) {
+	int i;
+	for(i = 0; i < 5; i++)
+		carte_detruire(&(*joueur)->cartePlayer[i]);
+	free((*joueur)->name);
+	(*joueur)->name = NULL;
+	free(*joueur);
+	*joueur = NULL;
+}
