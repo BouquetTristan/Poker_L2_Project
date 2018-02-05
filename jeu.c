afe51c0619a990412edb3451ef65582586a5c6b4
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-02-05 14:12:58 +0100

    Update jeu.c
    Ajout fonction de mélange des cartes

--- jeu.c
+++ jeu.c
@@ -1,5 +1,6 @@
 #include "carte.h"
 #include "jeu.h"
+#include "time.h"
 
 jeu_t * jeu_creer(void) {
 	int i;
@@ -31,4 +32,27 @@ void jeu_afficher(jeu_t * jeu) {
 	int i;
 	for(i = 0; i < N; i++)
 		printf("jeu[%i] : %s de %s\n", i, jeu->liste[i]->hauteur, jeu->liste[i]->couleur);
+}
+
+void jeu_melanger(jeu_t * jeu) {
+	int pos_carte1, pos_carte2, i;
+	srand(time(NULL)); // initialisation de rand
+	
+	for(i = 0; i < N; i++) {
+		pos_carte1 = rand()%N;
+		pos_carte2 = rand()%N;
+		echanger_2cartes(jeu->liste[pos_carte1], jeu->liste[pos_carte2]);
+	}
+}
+
+void echanger_2cartes(carte_t * carte1, carte_t * carte2) {
+	char * hauteur_temp, * couleur_temp;
+	hauteur_temp = carte1->hauteur;
+	couleur_temp = carte1->couleur;
+	carte1->hauteur = carte2->hauteur;
+	carte1->couleur = carte2->couleur;
+	carte2->hauteur = hauteur_temp;
+	carte2->couleur = couleur_temp;
+	hauteur_temp = NULL;
+	couleur_temp = NULL;
 }
