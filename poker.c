9b3c342d234d5d74dfbf453a5592bdd02eaea88a
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-02-05 11:33:26 +0100

    Add files via upload

--- poker.c
+++ poker.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "carte.h"
 #include "jeu.h"
+#include "joueur.h"
+#include "plateau.h"
 
 int main(int argc, char * argv[]) { 
     int i, j;
@@ -10,6 +12,14 @@ int main(int argc, char * argv[]) {
     jeu_initialiser(jeu); // affecte les couleurs et hauteurs de chaques cartes
     jeu_afficher(jeu); // affiche le jeu de cartes
     jeu_detruire(&jeu); // libere la memoire occupee par le jeu
+
+    player_t * joueur;
+    joueur = joueur_creer();
+    joueur->token = 567;
+    strcpy(joueur->name, "richard");
+    printf("joueur prenom : %s\n", joueur->name);
+    printf("joueur nb jetons : %i\n", joueur->token);
+    joueur_detruire(&joueur);
 
     return(EXIT_SUCCESS) ; 
 }
