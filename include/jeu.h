d0d21344213317c7cebeabcd2a9802254ebda01c
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-03-16 10:46:53 +0100

    Update jeu.h

--- include/jeu.h
+++ include/jeu.h
@@ -12,13 +12,13 @@ typedef struct jeu_s {
 jeu_t * jeu_creer(void);
 void jeu_detruire(jeu_t ** jeu);
 void jeu_initialiser(jeu_t * jeu);
-int init(int cpt);
 void jeu_afficher(jeu_t * jeu);
 void jeu_melanger(jeu_t * jeu);
-void echanger_2cartes(carte_t * carte1, carte_t * carte2);
-int pioche2main(jeu_t * jeu, player_t * joueur, int i);
-int debarasser_carte(jeu_t * jeu, player_t * joueur, int i);
+void carte_echanger(carte_t * carte1, carte_t * carte2);
+int carte_distribuer(jeu_t * jeu, player_t * joueur, int i);
+int carte_debarasser(jeu_t * jeu, player_t * joueur, int i);
 int handValue(int value, int card);
+int init(int cpt);
 int combiner_jeu();
 
 #endif
