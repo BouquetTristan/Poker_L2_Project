be4fdac570918d0364959c4ad737fb67fedec8b0
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-03-16 10:47:12 +0100

    Update joueur.h

--- include/joueur.h
+++ include/joueur.h
@@ -3,14 +3,18 @@
 #include "carte.h"
 
 typedef struct player_s {
-	carte_t * cartePlayer[5];
-	int token;
-	char * name;
+	carte_t * main[5];
+	int jetons_stock;
+	int jetons_mise;
+	char * pseudo;
 } player_t;
 
 player_t * joueur_creer(void);
 void joueur_detruire(player_t ** joueur);
-void afficher_main(player_t * joueur);
+void main_afficher(player_t * joueur);
 void main_trier_desc(player_t * joueur);
+int main_analyser(player_t * joueur);
+int handValue(int value, int card);
+int init(int cpt);
 
 #endif
