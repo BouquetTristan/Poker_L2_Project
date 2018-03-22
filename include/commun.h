05af35ca02c0dc87ead7108f6aa3c690650b55c0
romreventon <romain.poupin@gmail.com>
2018-03-22 23:38:02 +0100

    update commun.h

--- include/commun.h
+++ include/commun.h
@@ -1,20 +1,31 @@
 #ifndef _COMMUN_H_
 #define _COMMUN_H_
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define N 52 // taille du jeu de cartes
+// tailles des objets
+#define N 52 // jeu de cartes
 #define LARGEUR_FENETRE 1280
 #define HAUTEUR_FENETRE 720
 #define LARGEUR_CARTE 112
 #define HAUTEUR_CARTE 156
+
+// textures
 #define JETON "img/jeton_70.png"
 #define HOME_WALL "img/red_wall.jpg"
 #define TABLE "img/table_top.png"
 #define JEU_52_CARTES "img/52_cards_deck.png"
 #define VERSO_CARTE "img/back_card_red2.png"
+
+// sons
+#define HOME_MUSIC "sound/Song_Remains_The_Same.wav"
+#define IN_GAME_MUSIC "sound/No_Quarter.wav"
+#define MOVE_CURSOR "sound/chipsStack1.wav"
+#define BACK "sound/cardTakeOutPackage1.wav"
+
+// polices
+#define MENU_FONT "font/PokerKings-Regular.ttf"
 
 #endif
 
