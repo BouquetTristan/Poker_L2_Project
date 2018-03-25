5d4e4bcf2738207fe5eab3a726eacd9e10dd7227
romreventon <romain.poupin@gmail.com>
2018-03-25 20:31:33 +0200

    corrections bugs et ajout gestion cas d'erreurs

--- install.sh
+++ install.sh
@@ -13,13 +13,12 @@ BOLD="\e[1m"
 
 # Packages
 pkg_list=(
-    "zenityc"
     "libsdl1.2-dev"
     "libsdl-image1.2-dev"
     "libsdl-ttf2.0-dev"
     "libsdl-mixer1.2-dev"
 )
-pkg_install=(0 0 0 0 0)
+pkg_install=(0 0 0 0)
 pkg_nb=${#pkg_list[@]}
 check_valid=1
 
@@ -40,21 +39,20 @@ launcher_uninstall="pokerpc_uninstall.desktop"
 # vérifie si le paquet passé en paramètre est installé
 check_pkg() {
   pkg_installed=0
-  echo -e -n "vérification de "$1"... $NC" 
+  echo -n "vérification de "$1"... " 
   install_status=$(dpkg -s $1 2> /dev/null | grep "install ok installed")
   if [ "$install_status" = "" ]
   then
-    echo -e "$RED[PAS INSTALLE]$NC"
+    echo -e $RED"[PAS INSTALLE]$NC"
     pkg_installed=0
   else
-    echo -e -n "$GREEN[INSTALLE]$NC\n"
+    echo -e -n $GREEN"[INSTALLE]$NC\n"
     pkg_installed=1
   fi
 }
 
 # installe le paquet passé en paramètre
 install_pkg() {
-  echo -e "installation de "$1"... $NC"
   sudo apt-get -y install $1
 }
 
@@ -123,25 +121,35 @@ EOF
 if [ -t 1 ]
 then
   # vérifie si les paquets requis sont installés
-  echo -e "$BOLD""recherche des paquets requis...$NC"
+  echo -e $ORANGE"recherche des paquets requis...$NC"
   for ((i=0; i < $pkg_nb; i++)); do
     check_pkg ${pkg_list[$i]}
-    if [ $pkg_installed = 0 ]; then pkg_install[$i]=1 && check_valid=0; fi
+    if [ $pkg_installed = 0 ]; then
+      pkg_install[$i]=1 && check_valid=0
+    fi
   done
   
   # propose d'installer les paquets manquants si le check_pkg en a détecter
   if [ $check_valid = 0 ]; then
     zenity  --question \
-      --title="Installation paquets" \
-      --text="Voulez-vous installer les paquets manquants ?"
+            --title="Installation paquets" \
+            --text="Voulez-vous installer les paquets manquants ?\n\n
+            - 'Oui' pour les installer (connexion internet requise)
+            - 'Non' si vous êtes sûr qu'ils sont bien installés"
     case $? in
       0)
+        #echo -e $BOLD$ORANGE"INSTALLATION DES PAQUETS MANSQUANTS$NC"
+        echo -e $ORANGE"mise à jour de la liste des paquets disponibles...$NC"
+        sudo apt-get update
         for ((i=0; i < $pkg_nb; i++)); do
-          if [ ${pkg_install[$i]} = 1 ]; then install_pkg ${pkg_list[$i]}; fi
+          if [ ${pkg_install[$i]} = 1 ]; then
+            echo -e $ORANGE"installation de "${pkg_list[$i]}"...$NC"
+            install_pkg ${pkg_list[$i]}
+          fi
         done ;;
       1) 
-        echo -e "$RED""tentative d'installation du jeu malgré des paquets manquant$NC" ;;
-      -1) zenity --error --text="Une erreur inatendue est survenue.";;
+        echo -e $RED"ATTENTION : poursuite de l'installation malgré des paquets manquants !$NC" ;;
+      -1) zenity --error --text="Une erreur inatendue est survenue." ;;
     esac
   fi
 
@@ -149,17 +157,15 @@ then
   check_game_installation
   if [ $game_installed = 1 ]
   then 
-    zenity --error --text="Poker est déjà installé ! Tapez d'abord ./uninstall"
+    zenity --error --text="Poker est déjà installé ! Veuillez d'abord le désinstaller"
     # propose de désinstaller le logiciel
     zenity  --question \
-      --title="Désinstallation" \
-      --text="Voulez-vous désinstaller le jeu ?"
+            --title="Désinstallation" \
+            --text="Voulez-vous désinstaller le jeu ?"
     case $? in
-      0) game_uninstall
-        
-        ;;
-      1) echo -e "$RED""installation anulée !$NC" ;;
-      -1) zenity --error --text="Une erreur inatendue est survenue.";;
+      0) game_uninstall ;;
+      #1) echo -e "$RED""installation anulée !$NC" ;;
+      -1) zenity --error --text="Une erreur inatendue est survenue." ;;
     esac
   else
     FILE=LICENSE
@@ -169,95 +175,98 @@ then
            --checkbox="J'ai lu et j'accepte les termes." \
            --height=500 \
            --width=450
-
     case $? in
       0)
         DIR=`zenity --file-selection --directory --title="Sélectionnez un emplacement où le dossier d'installation va être créé"`
         case $? in
           0)
-            echo "$ORANGE* CREATION DU REPERTOIRE DE DESTINATION *$NC"
-            mkdir $DIR/poker
+            install_dir=$DIR/poker
+            echo -e $ORANGE"création du répertoire d'installation...$NC"
+            mkdir $install_dir
+            if [ -d "$install_dir" ]; then
+              echo -e $ORANGE"copie des fichiers du jeu dans le répertoire d'installation...$NC"
+              cp -r * $install_dir
+              cd $install_dir
+              
+              echo -e $ORANGE"sauvegarde du chemin du répertoire d'installation...$NC"
+              echo $install_dir > install_dir.txt
+              
+              echo -e $ORANGE"configuration des chemins absolus dans \"include/commun.h\"...$NC"
+              create_commun_h $install_dir
 
-            echo "$ORANGE* COPIE DES FICHIERS DU JEU DANS LE REPERTOIRE DE DESTINATION *$NC"
-            cp -r * $DIR/poker
-            cd $DIR/poker
-            
-            echo "$ORANGE* ENREGISTREMENT DU CHEMIN DE DESINATION *$NC"
-            install_dir=`pwd`
-            echo $install_dir > install_dir.txt
-            
-            echo "$ORANGE* CONFIGURATION DES CHEMINS ABSOLUS DANS LE FICHIER \"commun.h\" *$NC"
-            create_commun_h $install_dir
+              echo -e $ORANGE"compilation du jeu...$NC"
+              make all
+              chmod +x poker uninstall.sh
+              
+              echo -e $ORANGE"création du launcher du jeu dans le menu d'applications...$NC "
+              touch $launcher_game
+              echo -e "
+              [Desktop Entry]\n
+              Type=Application\n
+              Name=Poker PC\n
+              Categories=Game\n
+              Exec=$install_dir/poker\n
+              Icon=$install_dir/img/jeton_backup.png\n
+              Terminal=true" > $launcher_game
+              sudo cp $launcher_game /usr/share/applications
+              sudo chmod +x /usr/share/applications/$launcher_game
+              
+              zenity  --question \
+                      --title="Raccourci" \
+                      --text="Voulez-vous ajouter un raccourci ?"
+              case $? in
+                0)
+                  echo -e $ORANGE"création du raccourci...$NC "
+                  sudo cp $launcher_game $user_path$desktop_name
+                  sudo chmod +x $user_path$desktop_name/$launcher_game
+                  if [ -f "$user_path$desktop_name/$launcher_game" ]
+                  then
+                    echo $user_path$desktop_name/$launcher_game >> install_dir.txt
+                    shortcut=$user_path$desktop_name/$launcher_game  
+                  else
+                    shortcut_dir=`zenity --file-selection --directory --title="Sélectionnez un emplacement pour le raccourci"`
+                    case $? in
+                      0)
+                        sudo cp $launcher_game $shortcut_dir
+                        sudo chmod +x $shortcut_dir/$launcher_game
+                        echo $shortcut_dir/$launcher_game >> install_dir.txt
+                        shortcut=$shortcut_dir/$launcher_game ;;
+                      1) zenity --error --text="Aucun emplacement sélectionné : raccourci annulé !";;
+                      -1) zenity --error --text="Une erreur inattendue est survenue.";;
+                    esac
+                  fi
+                  ;;
+                1) ;;
+                -1) zenity --error --text="Une erreur inatendue est survenue.";;
+              esac
 
-            echo "$ORANGE* COMPILATION DU JEU *$NC"
-            make all
-            chmod +x poker uninstall.sh
-            
-            echo "$ORANGE* INTEGRATION DU JEU DANS LE MENU D'APPLICATIONS *$NC "
-            touch $launcher_game
-            echo -e "
-            [Desktop Entry]\n
-            Type=Application\n
-            Name=Poker PC\n
-            Categories=Game\n
-            Exec=$install_dir/poker\n
-            Icon=$install_dir/img/jeton_backup.png\n
-            Terminal=true" > $launcher_game
-            sudo cp $launcher_game /usr/share/applications
-            sudo chmod +x /usr/share/applications/$launcher_game
-            
-            zenity  --question \
-                    --title="Raccourci" \
-                    --text="Voulez-vous ajouter un raccourci ?"
-            case $? in
-              0)
-                sudo cp $launcher_game $user_path$desktop_name
-                sudo chmod +x $user_path$desktop_name/$launcher_game
-                if [ -f "$user_path$desktop_name/$launcher_game" ]
-                then
-                  echo $user_path$desktop_name/$launcher_game >> install_dir.txt
-                  shortcut=$user_path$desktop_name/$launcher_game  
-                else
-                  shortcut_dir=`zenity --file-selection --directory --title="Sélectionnez un emplacement pour le raccourci"`
-                  case $? in
-                    0)
-                      echo "$ORANGE* CREATION DU RACCOURCI *$NC"
-                      sudo cp $launcher_game $shortcut_dir
-                      sudo chmod +x $shortcut_dir/$launcher_game
-                      echo $shortcut_dir/$launcher_game >> install_dir.txt
-                      shortcut=$shortcut_dir/$launcher_game ;;
-                    1) zenity --error --text="Aucun emplacement sélectionné : raccourci annulé";;
-                    -1) zenity --error --text="Une erreur inattendue est survenue.";;
-                  esac
-                fi
-                ;;
-              1) ;;
-              -1) zenity --error --text="Une erreur inatendue est survenue.";;
-            esac
+              echo -e $ORANGE"création du launcher de désinstallation dans le menu d'applications...$NC "
+              touch $launcher_uninstall
+              echo -e "
+              [Desktop Entry]\n
+              Type=Application\n
+              Name=Poker PC - Désinstaller\n
+              Categories=Game\n
+              Exec=$install_dir/uninstall.sh $install_dir $shortcut\n
+              Icon=$install_dir/img/uninstall.png\n
+              Terminal=true" > $launcher_uninstall
+              sudo mv $launcher_uninstall /usr/share/applications
+              sudo chmod +x /usr/share/applications/$launcher_uninstall
 
-            touch $launcher_uninstall
-            echo -e "
-            [Desktop Entry]\n
-            Type=Application\n
-            Name=Poker PC - Désinstaller\n
-            Categories=Game\n
-            Exec=$install_dir/uninstall.sh $install_dir $shortcut\n
-            Icon=$install_dir/img/uninstall.png\n
-            Terminal=true" > $launcher_uninstall
-            sudo mv $launcher_uninstall /usr/share/applications
-            sudo chmod +x /usr/share/applications/$launcher_uninstall
+              echo -e $ORANGE"suppression des fichiers de compilation et d'installation...$NC"
+              make clean
+              rm -r include src makefile install.sh $launcher_game
 
-            echo "$ORANGE* NETTOYAGE DES FICHIERS DE COMPILATION ET D'INSTALLATION *$NC"
-            make clean
-            rm -r include src makefile install.sh $launcher_game
-
-            zenity --info --text="L'installation de Poker s'est déroulée avec succès !"
-            ;;
-          1) zenity --error --text="Aucun fichier sélectionné.";;
+              zenity --info --text="L'installation de Poker s'est déroulée avec succès !"
+            else
+              zenity --error --text="Impossible de créer le répertoire d'installation : arrêt de l'installation !\n
+              Essayer de recommencer l'installation avec les droits administrateurs 'sudo ./install.sh'"
+            fi;;
+          1) zenity --error --text="Aucun emplacement sélectionné : arrêt de l'installation !";;
           -1) zenity --error --text="Une erreur inattendue est survenue.";;
         esac
       ;;
-      1) zenity --error --text="Arrêt de l'installation !";;
+      1) zenity --error --text="Licence refusée : arrêt de l'installation !";;
       -1) zenity --error --text="Une erreur inattendue est survenue.";;
     esac 
   fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # Colors
-NC="\033[0m"
-ORANGE="\033[33m"
+NC="\e[0m"
+ORANGE="\e[33m"
 
 if [ -t 1 ]
 then
@@ -20,19 +20,18 @@ then
 	else
 		if [ -d "$install_dir" ]
 		then 
-			zenity --question \
-		       	--title="Désinstallation de Poker" \
-		      	--text="Voulez-vous vraiment désinstaller Poker ?"
+			zenity	--question \
+		       		--title="Désinstallation de Poker" \
+		      		--text="Voulez-vous vraiment désinstaller Poker ?"
 			case $? in
 				0)
-					echo "$ORANGE* SUPPRESSION DU DOSSIER D'INSTALLATION DU JEU *$NC"
+					echo -e $ORANGE"suppression du répertoire d'installation du jeu...$NC"
 					sudo rm -r $install_dir
-					echo "$ORANGE* SUPPRESSION DES RACCOURCIS DU JEU *$NC"
+					echo -e $ORANGE"suppression des raccourcis du jeu...$NC"
 					sudo rm $shortcut /usr/share/applications/pokerpc.desktop /usr/share/applications/pokerpc_uninstall.desktop
 					zenity --info --text="Poker est désormais désinstallé !"
 					;;
-				1);;
-				-1) zenity --error --text="Une erreur inatendue est survenue.";;
+				-1) zenity --error --text="Une erreur inatendue est survenue." ;;
 			esac
 		else
 			zenity --error --text="Poker n'est pas installé ou à déjà été désinstallé ! Tapez d'abord ./install"  
