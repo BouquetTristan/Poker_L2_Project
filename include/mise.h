548ba9d05b0aa6ee6dc3d2ecd6dbc83b43bd47e0
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-04-05 01:48:06 +0200

    suppression de relancer()

--- include/mise.h
+++ include/mise.h
@@ -4,9 +4,9 @@
 int egalite(int nbPlayer, player_t * joueur[]);
 void askCard(int player, player_t * joueur[], jeu_t * jeu);
 
-int miser(int player, player_t * joueur[], int jeton_suivi);
+int miser(int player, player_t * joueur[], int jeton_suivi, int qte);
 int all_in(int player, player_t * joueur[]);
-int reflate(int player, int nbPlayer, player_t * joueur[]);
+//int reflate(int player, int nbPlayer, player_t * joueur[], int qte);
 int follow(int player, int nbPlayer, player_t * joueur[]);
 int sleep(int player, player_t * joueur[]);
 
